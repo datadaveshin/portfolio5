49ba0f38238eec8e98fb37a169809079fa2e3753
David Shin <davidshin444@gmail.com>
2018-11-14 22:15:36 -0800

    Integrated new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,23 @@ class PortfoliosController < ApplicationController
     # Then the Contoller makes @portfolio_items available to the View
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  # This part will be different from the blogs one, as we will tell it what we can use for security
+  # The `.permit` is a whitelisted set of parameters
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html {redirect_to portfolios_path, notice: 'Your portfolio item is now live'}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
 end
